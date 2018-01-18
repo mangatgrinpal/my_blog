ca7d11ee20afe084f50be5fbd0344ed71d002388
Ginny Mangat <ginnymangat@Ginnys-MacBook-Air.local>
2018-01-18 14:22:06 -0800

    Add comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+	has_many :comments, dependent: :destroy #making association to post. if post gets deleted, comment gets deleted from database as well.
 	validates :title, presence: true, length: { minimum: 5 }
 	validates :body, presence: true
 end
